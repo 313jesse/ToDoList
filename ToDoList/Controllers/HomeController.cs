30ccdcdfb30ed4833a32cd79ace16f2c5019d729
unknown <jess7642@gmail.com>
2018-08-29 08:44:28 -0400

    A web ToDo List

---
+++ ToDoList/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToDoList.Models;
+
+
+namespace ToDoList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ToDoListEntities ORM = new ToDoListEntities();
+            ViewBag.newtask = ORM.Tasks.ToList();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+
+        }
+
+        public ActionResult NewTask()
+        {
+            return View();
+        }
+
+        public ActionResult SaveNewTask(Task newtask)
+        {
+            ToDoListEntities ORM = new ToDoListEntities();
+            ORM.Tasks.Add(newtask);
+            ORM.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteTask(int taskID)
+        {
+            ToDoListEntities ORM = new ToDoListEntities();
+            Task TaskToDelete = ORM.Tasks.Find(taskID);
+
+            ORM.Tasks.Remove(TaskToDelete);
+            ORM.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
+        
+
+
+
+    }
+}
